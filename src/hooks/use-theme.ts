286c9b8a672fe0fd0765112410d10d2cf4d95c19
Andrew <myatkyawthu4002@gmail.com>
2025-06-20 18:00:34 +0630

    Create use-theme.ts

---
+++ src/hooks/use-theme.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { useTheme as useNextTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function useTheme() {
+  const { theme, setTheme, resolvedTheme } = useNextTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return {
+    theme,
+    setTheme,
+    resolvedTheme,
+    mounted,
+    isDark: resolvedTheme === "dark",
+    isLight: resolvedTheme === "light",
+    isSystem: theme === "system",
+  };
+}
