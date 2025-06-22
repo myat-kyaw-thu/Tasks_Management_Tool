872116b4779656e8baec53e3a8fef569b634e8e0
Andrew <myatkyawthu4002@gmail.com>
2025-06-22 09:32:42 +0630

    Emuns

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -295,3 +295,14 @@ export type TablesUpdate<
   ? U
   : never
   : never;
+
+export type Enums<
+  PublicEnumNameOrOptions extends keyof Database["public"]["Enums"] | { schema: keyof Database; },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database; }
+  ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+  : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database; }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never;
