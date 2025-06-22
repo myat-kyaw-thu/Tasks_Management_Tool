e4b7bc67254e543e4d18f391a0717ad396064f9f
Andrew <myatkyawthu4002@gmail.com>
2025-06-22 09:32:28 +0630

    TablesUpdate

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -275,3 +275,23 @@ export type TablesInsert<
   ? I
   : never
   : never;
+
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends keyof Database["public"]["Tables"] | { schema: keyof Database; },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database; }
+  ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database; }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+    Update: infer U;
+  }
+  ? U
+  : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+    Update: infer U;
+  }
+  ? U
+  : never
+  : never;
