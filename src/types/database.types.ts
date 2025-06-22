823f3e5d053245c0a20338758316cc526cb81a98
Andrew <myatkyawthu4002@gmail.com>
2025-06-22 09:32:18 +0630

    TablesInsert

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -256,3 +256,22 @@ export type Tables<
   ? R
   : never
   : never;
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends keyof Database["public"]["Tables"] | { schema: keyof Database; },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database; }
+  ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database; }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+    Insert: infer I;
+  }
+  ? I
+  : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+    Insert: infer I;
+  }
+  ? I
+  : never
+  : never;
